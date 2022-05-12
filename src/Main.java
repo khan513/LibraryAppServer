cbf22b4be14fb72da0ad55e3acb6854de700be90
khan513 <abylaikhan513@gmail.com>
2022-05-12 10:46:15 +0600

    minor updates

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ import com.example.library.net.Server;
 
 import java.io.IOException;
 import java.net.ServerSocket;
-
 public class Main {
     public static void main(String[] args) {
         try (ServerSocket serverSocket = new ServerSocket(1234)) {
